8d2578d782927453d033b865d031133737b47482
bohdan <bohdan.gibert@gmail.com>
2020-04-10 18:06:22 +1000

    Configure husky and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,25 @@
-import * as React from "react";
-import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
-import {
-  CssBaseline,
-  AppBar,
-  Toolbar,
-  Typography,
-  Container
-} from "@material-ui/core";
-import { LazyRoute } from "./routes";
+import * as React from 'react';
+import { Redirect, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { AppBar, Container, CssBaseline, Toolbar, Typography } from '@material-ui/core';
+import { LazyRoute } from './routes';
 
-export const App: React.FunctionComponent = () => {
-  return (
+export const App: React.FunctionComponent = () => (
     <>
-      <CssBaseline />
-      <Router>
-        <AppBar position="relative">
-          <Toolbar>
-            <Typography variant="h6">Testing</Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          <Container>
-            <Switch>
-              <LazyRoute
-                path="/pokemon"
-                loader={() => import("./pages/PokemonListPage")}
-              />
-              <Redirect to="/pokemon" />
-            </Switch>
-          </Container>
-        </main>
-      </Router>
+        <CssBaseline />
+        <Router>
+            <AppBar position="relative">
+                <Toolbar>
+                    <Typography variant="h6">Testing</Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <Container>
+                    <Switch>
+                        <LazyRoute path="/pokemon" loader={() => import('./pages/PokemonListPage')} />
+                        <Redirect to="/pokemon" />
+                    </Switch>
+                </Container>
+            </main>
+        </Router>
     </>
-  );
-};
+);
